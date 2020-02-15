2d74c75e678649a2cc3c9f356f72de2e62653849
Daehwan Cho <daehwancho@Daehwanui-Macmini.local>
2020-02-16 04:58:51 +0900

    movies api and movies

---
+++ screens/Movies/MoviesContainer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MoviesPresenter from "./MoviesPresenter";
+
+export default class extends React.Component {
+    state = {
+        loading: true
+    };
+
+    render() {
+        const { loading } = this.state;
+        return <MoviesPresenter loading={loading} />;
+    }
+}
