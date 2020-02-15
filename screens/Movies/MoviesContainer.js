c8b1467ec396a2e04ccd402064b7b6028c8abeab
Daehwan Cho <daehwancho@Daehwanui-Macmini.local>
2020-02-16 05:58:37 +0900

    home container part one

--- screens/Movies/MoviesContainer.js
+++ screens/Movies/MoviesContainer.js
@@ -1,10 +1,29 @@
 import React from "react";
 import MoviesPresenter from "./MoviesPresenter";
+import { movies } from "../../api";
 
 export default class extends React.Component {
     state = {
-        loading: true
+        loading: true,
+        upcoming: null,
+        popular: null,
+        nowPlaying: null,
+        error: null
     };
+
+    async componentDidMount() {
+        try {
+            const upcoming = await movies.getUpcoming();
+            const popular = await movies.getPopular();
+            const nowPlaying = await movies.getNowPlaying();
+            console.log("contents: ", upcoming, popular, nowPlaying);
+        } catch {
+            this.setState({ error: "Can't get Movies." });
+        } finally {
+            this.setState({ loading: false });
+            console.log("movies loading finish");
+        }
+    }
 
     render() {
         const { loading } = this.state;
